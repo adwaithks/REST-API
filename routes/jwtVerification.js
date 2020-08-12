76d956619b26728807a9e64467b76004816fdcd0
Adwaith <45693088+adwaithks@users.noreply.github.com>
2020-08-12 07:51:38 -0700

    Update jwtVerification.js

--- routes/jwtVerification.js
+++ routes/jwtVerification.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 module.exports = function(req, res, next) {
 
   try {
-    const accessToken = req.header('Access-Token');
+    const accessToken = req.header('Access-Token').split(' ')[1];
     jwt.verify(accessToken, process.env.JWT_SECRET, (err, decoded) => {
       if (err) return res.status(403).json('Invalid Authorization Token');
       req.user = decoded;
